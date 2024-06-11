8f657f7b78a5d2ce5950e730f617d381ebea5c4d
Yurii <yuriibel888@gmail.com>
2024-06-11 19:36:42 +0300

    fix

--- app/ui/tap/tap-button/tap.tsx
+++ app/ui/tap/tap-button/tap.tsx
@@ -7,10 +7,6 @@ export function Tap() {
   const increment = useStore((state) => state.increment);
   const [isActive, setIsActive] = useState(false);
 
-  let isMobile = /iPhone|iPod|iPad|Android|BlackBerry/.test(
-    navigator.userAgent
-  );
-
   const handleMouseDown = () => {
     setIsActive(true);
   };
@@ -22,7 +18,6 @@ export function Tap() {
   const handleClick = () => {
     increment();
     setIsActive(true);
-    navigator.vibrate(10);
     setTimeout(() => setIsActive(false), 10);
   };
 
@@ -34,25 +29,13 @@ export function Tap() {
       className="transition-transform ease-in-out duration-100"
     >
       <div
-        className={`flex justify-center items-center border-2 border-black rounded-full bg-orange-200 w-[380px] h-[380px] p-8 transition-transform duration-100 ease-in-out ${
+        className={`h-full flex flex-col justify-between items-center  p-6 transition-transform duration-100 ease-in-out ${
           isActive && "scale-95"
         }`}
       >
-        {/* <div
-          className={`h-full flex flex-col justify-between items-cente p-6 transition-transform duration-100 ease-in-out ${
-            isActive && "scale-95"
-          }`}
-        > */}
-        <Image
-          // className={`h-full flex flex-col justify-between items-cente p-2 transition-transform duration-100 ease-in-out ${
-          //   isActive && "scale-95"
-          // }`}
-          src={`/cat-2.png`}
-          alt="cat-face"
-          width="320"
-          height="320"
-        />
-        {/* </div> */}
+        <div className="flex justify-center items-center border-2 border-black rounded-full bg-orange-200 w-[380px] h-[380px] p-8">
+          <Image src={`/cat-2.png`} alt="cat-face" width="320" height="320" />
+        </div>
       </div>
     </button>
   );
