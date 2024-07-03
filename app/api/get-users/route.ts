9e2053ae9672c21e295b659e4cc2be5d96f0cf54
Yurii <yuriibel888@gmail.com>
2024-07-03 15:56:23 +0300

    refactor: fixed route get-users

--- app/api/get-users/route.ts
+++ app/api/get-users/route.ts
@@ -1,12 +1,9 @@
-// pages/api/get-users.ts
-
-import type { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   if (req.method === "GET") {
     try {
       const users = await prisma.user.findMany();
@@ -14,9 +11,9 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
       return NextResponse.json(users);
     } catch (error) {
       console.error("Error fetching users:", error);
-      res.status(500).json({ message: "Failed to fetch users" });
+      NextResponse.json({ message: "Failed to fetch users" });
     }
   } else {
-    res.status(405).json({ message: "Method not allowed" });
+    NextResponse.json({ message: "Method not allowed" });
   }
 }
